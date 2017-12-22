78aaf58f1897a60ec2c750c580ce04e4ec5a429c
HappyDays-jQuery <developer@jquery.tokyo>
2017-12-22 14:53:21 +0900

    normalizeテスト

---
+++ tests/Utils/UtilsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SiteMap;
+
+class FileIoTest extends CommonTestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testNormalize()
+    {
+        $this->assertEquals("aaa/bbb", Utils::normalize('aaa/bbb'));
+        $this->assertEquals("aaa/bbb", Utils::normalize('aaa//bbb'));
+        $this->assertEquals("aaa/bbb", Utils::normalize('aaa///bbb'));
+        $this->assertEquals("/aaa/bbb", Utils::normalize('/aaa///bbb'));
+        $this->assertEquals("/aaa/bbb", Utils::normalize('//aaa///bbb'));
+        $this->assertEquals("/aaa/bbb", Utils::normalize('///aaa///bbb'));
+    }
+
+    public function testNormalizeRelativePath()
+    {
+        $this->assertEquals("bbb", Utils::normalize('aaa/../bbb'));
+        $this->assertEquals("../bbb", Utils::normalize('aaa/../../bbb'));
+        $this->assertEquals("../../bbb", Utils::normalize('aaa/../../../bbb'));
+        $this->assertEquals("../../bbb", Utils::normalize('aaa//..//..//..//bbb'));
+        $this->assertEquals("../../ccc", Utils::normalize('aaa//..//..//..//bbb/../ccc'));
+    }
+
+    public function testNormalizeCurrentPath()
+    {
+        $this->assertEquals("aaa/bbb", Utils::normalize('aaa/./bbb'));
+        $this->assertEquals("bbb", Utils::normalize('./bbb'));
+        $this->assertEquals("aaa/bbb/ccc", Utils::normalize('aaa/./bbb/./ccc'));
+    }
+
+    public function testNormalizeAbsolutePath()
+    {
+        $this->assertEquals("/aaa/bbb", Utils::normalize('/aaa/./bbb'));
+        $this->assertEquals("/bbb", Utils::normalize('/aaa/../bbb'));
+        $this->assertEquals("/bbb", Utils::normalize('/aaa/../../bbb'));
+        $this->assertEquals("/bbb", Utils::normalize('/aaa/../../../bbb'));
+    }
+}
